0601988e116f7b6ebacab69fb57460e9ddcb2542
Philippe LE <le.philippe.duc.tai@gmail.com>
2024-04-24 22:29:24 +0200

    Added problem003 solved.

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ pub mod problems;
 fn main() {
     problems::problem001::run();
     problems::problem002::run();
+    problems::problem003::run();
 }
---
+++ src/problems/problem003.rs
@@ -0,0 +1,23 @@
+fn prime_factors(n: u128) -> Vec<u128> {
+    let mut p = n;
+    let limit = (n as f64).sqrt() as u128;
+    let mut k = 2;
+    let mut result: Vec<u128> = vec![];
+    while k < limit {
+        if p % k == 0 {
+            result.push(k);
+            while p % k == 0 {
+                p = p / k;
+            }
+        }
+        k += 1;
+    }
+    return result;
+}
+
+pub fn run() {
+    const VALUE: u128 = 600851475143;
+    let res = prime_factors(VALUE);
+    let max_value = res.iter().max();
+    println!("{:?}", max_value)
+}
