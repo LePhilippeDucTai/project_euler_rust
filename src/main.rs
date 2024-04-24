e1d20385bc2676a75967d950ef2683e9d9329a57
Philippe LE <le.philippe.duc.tai@gmail.com>
2024-04-24 09:52:34 +0200

    Added file structure.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod problems;
+
 fn main() {
-    println!("Hello, world!");
+    problems::problem001::run();
 }
---
+++ src/problems/problem001.rs
@@ -0,0 +1,14 @@
+fn multiple_of_3_or_5(x: &i64) -> bool {
+    (x % 5 == 0) | (x % 3 == 0)
+}
+
+fn solve(n_max: i64) -> i64 {
+    let range = 1..n_max;
+    let filtered = range.filter(multiple_of_3_or_5);
+    filtered.sum()
+}
+
+pub fn run() {
+    let result = solve(1000);
+    println!("{:?}", result);
+}
